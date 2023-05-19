2278cdadf401f0fbec9fd772ae4cc87ef9dcdbe6
abdullah nadim <nadim.soft.111@gmail.com>
2023-05-19 18:55:25 +0600

    Graph

---
+++ Graph -unweighted.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+int graph[100][100];
+void initial(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+            {
+                graph[i][j]=0;
+            }
+    }
+}
+
+void input()
+{
+    int s,d,e;
+    cout<<"Number of edges: \n";
+    cin>>e;
+    while(e--){
+    cout<<"Enter source: \n";
+    cin>>s;
+    cout<<"Enter destination: \n";
+    cin>>d;
+    graph[s][d]=1;
+    }
+}
+
+void print(int n)
+{
+    cout<<"The graph: \n";
+    cout<<"  \t";
+    for(int K=0;K<n;K++)
+        {
+            cout<<K<<"\t";
+        }
+        cout<<"\n_____________________________________________\n";
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<i<<"|\t";
+        for(int j=0;j<n;j++)
+            {
+                cout<<graph[i][j]<<"\t";
+            }
+            cout<<endl;
+    }
+}
+int main()
+{
+    int n;
+    cout<<"Number of vertices: \n";
+    cin>>n;
+    initial(n);
+    input();
+    print(n);
+}
---
+++ directed and weighted graph.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+int graph[100][100];
+void initial(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+            {
+                graph[i][j]=0;
+            }
+    }
+}
+
+void input()
+{
+    int s,d,e,w;
+    cout<<"Number of edges: \n";
+    cin>>e;
+    while(e--){
+    cout<<"Enter source: \n";
+    cin>>s;
+    cout<<"Enter destination: \n";
+    cin>>d;
+    cout<<"Enter weight: \n";
+    cin>>w;
+    graph[s][d]=w;
+    }
+}
+
+void print(int n)
+{
+    cout<<"The graph: \n";
+    cout<<"  \t";
+    for(int K=0;K<n;K++)
+        {
+            cout<<K<<"\t";
+        }
+        cout<<"\n_____________________________________________\n";
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<i<<"|\t";
+        for(int j=0;j<n;j++)
+            {
+                cout<<graph[i][j]<<"\t";
+            }
+            cout<<endl;
+    }
+}
+int main()
+{
+    int n;
+    cout<<"Number of vertices: \n";
+    cin>>n;
+    initial(n);
+    input();
+    print(n);
+}
